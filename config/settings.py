4f93d6bd474eaef458e6e3622750ab1a800f6992
hichamkhrbouch000-crypto <hichamkhrbouch000@gmail.com>
2026-07-07 19:24:58 +0100

    Delete config directory

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-API_KEY = os.getenv("EXCHANGE_API_KEY")
-SECRET = os.getenv("EXCHANGE_SECRET")
-SYMBOL = os.getenv("SYMBOL", "BTC/USDT")
